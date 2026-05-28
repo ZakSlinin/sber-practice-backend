02b53ee4b5f1768eff122b012fa11951ff7cb68f
Zak Slinin <156526564+ZakSlinin@users.noreply.github.com>
2026-05-28 12:00:31 +0300

    feat: tasks table

---
+++ db/migrations/000005_create_tasks_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE tasks (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    challenge_id UUID NOT NULL,
+    assigned_to  UUID NOT NULL,
+    title TEXT NOT NULL,
+    description TEXT,
+    is_daily BOOLEAN NOT NULL DEFAULT FALSE,
+    status task_status NOT NULL DEFAULT 'pending',
+    deadline TIMESTAMPTZ
+);
