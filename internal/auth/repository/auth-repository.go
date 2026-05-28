619d8356ecfe1b1f2ac9a70cf225a942f03d5809
Zak Slinin <156526564+ZakSlinin@users.noreply.github.com>
2026-05-28 15:18:42 +0300

    feat: auth-repository.go

---
+++ internal/auth/repository/auth-repository.go
@@ -0,0 +1,44 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+	"github.com/ZakSlinin/sber-practice-backend/internal/auth/models"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type AuthRepository interface {
+	Create(ctx context.Context, user *models.User) (*models.User, error)
+	GetByEmailAndWorkspace(ctx context.Context, email string, workspaceID uuid.UUID) (*models.User, error)
+}
+
+type PostgresAuthRepository struct {
+	db *gorm.DB
+}
+
+func NewAuthRepository(db *gorm.DB) *PostgresAuthRepository {
+	return &PostgresAuthRepository{db: db}
+}
+
+func (r *PostgresAuthRepository) Create(ctx context.Context, user *models.User) (*models.User, error) {
+	result := r.db.WithContext(ctx).Create(user)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return user, nil
+}
+
+func (r *PostgresAuthRepository) GetByEmailAndWorkspace(ctx context.Context, email string, workspaceID uuid.UUID) (*models.User, error) {
+	var user models.User
+	result := r.db.WithContext(ctx).Where("email = ? AND workspace_id = ?", email, workspaceID).Find(user)
+	if result.Error != nil {
+		if result.RowsAffected == 0 {
+			return nil, fmt.Errorf("user not found")
+		}
+		return nil, result.Error
+	}
+
+	return &user, nil
+}
