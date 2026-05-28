84c16e7b4d20bea8c846f84ffc2a81bf69ddf682
Zak Slinin <156526564+ZakSlinin@users.noreply.github.com>
2026-05-28 11:34:55 +0300

    feat: create users table

---
+++ db/migrations/000001_create_users_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+    id            UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    workspace_id  UUID NOT NULL REFERENCES workspaces(id) ON DELETE CASCADE,
+    email         TEXT NOT NULL,
+    password_hash TEXT NOT NULL,
+    name          TEXT NOT NULL,
+    role          user_role NOT NULL DEFAULT 'pm',
+    team          TEXT,
+    total_points  INT NOT NULL DEFAULT 0,
+
+    UNIQUE (workspace_id, email)
+);
