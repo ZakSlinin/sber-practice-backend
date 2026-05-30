03865b6cae14266bacf94a1a117b785ae4e9ada2
Zak Slinin <156526564+ZakSlinin@users.noreply.github.com>
2026-05-30 15:30:26 +0300

    feat: cors settings

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ZakSlinin/sber-practice-backend/internal/auth/repository"
 	"github.com/ZakSlinin/sber-practice-backend/internal/auth/service"
 	repository2 "github.com/ZakSlinin/sber-practice-backend/internal/workspace/repository"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
@@ -55,6 +56,14 @@ func main() {
 	authHandler := handler.NewAuthHandler(authService)
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: false,
+	}))
 
 	api := r.Group("/api")
 	{
