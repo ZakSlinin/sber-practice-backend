5ab53f200455e6150bc2fe95a4ff136e45ec6fa0
Zak Slinin <156526564+ZakSlinin@users.noreply.github.com>
2026-05-30 15:37:52 +0300

    feat: challenge model

---
+++ internal/challenges/models/challenge.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"github.com/google/uuid"
+)
+
+type Challenge struct {
+	ID          uuid.UUID
+	WorkspaceID uuid.UUID
+	Title       string
+	Description string
+	Level       int
+	IsActive    bool
+	CreatedBy   uuid.UUID
+}
